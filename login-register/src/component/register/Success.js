16f98fd55bf84ebb958fbfe423c9e2e5fba965d4
prasandsouza <dsouzaprasan@gmail.com>
2023-03-27 16:10:13 +0530

    solved bugs

--- login-register/src/component/register/Success.js
+++ login-register/src/component/register/Success.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 function Success() {
     let query = window.location.search.substring(1);
     console.log(query)//"app=article&act=news_content&aid=160990"
-    let vars = query.split("&");
-    let values = vars[1].split('=')
+    let vars = query ? query.split("&") : "";
+    let values = vars ? vars[1].split('=') : ''
     let paymentStatus = values[1]
     useEffect(() => {
         axios.post('https://instamojo.onrender.com/success',paymentStatus)
@@ -17,7 +17,7 @@ function Success() {
     }, [])
     return (
         <div>
-            <Link to='/' className='text-center text-blue-400 border w-full'><h1> Payment is successful back to home page </h1></Link>
+            {paymentStatus === 'Failed' ?  <Link to='/' className='text-center text-red-400 border w-full'><h1> Payment is Unsuccessful back to home page </h1></Link> : <Link to='/' className='text-center text-blue-400 border w-full'><h1> Payment is successful back to home page </h1></Link>}
         </div>
     )
 }
