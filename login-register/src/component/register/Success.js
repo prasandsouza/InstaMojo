a652df0eb2af21e17f264f4b5ada74e81977ba0e
prasandsouza <dsouzaprasan@gmail.com>
2023-03-27 16:16:36 +0530

    solved bugs

--- login-register/src/component/register/Success.js
+++ login-register/src/component/register/Success.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Link, useSearchParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 function Success() {
     let query = window.location.search.substring(1);
@@ -7,14 +7,14 @@ function Success() {
     let vars = query ? query.split("&") : "";
     let values = vars ? vars[1].split('=') : ''
     let paymentStatus = values[1]
-    useEffect(() => {
+    useEffect((paymentStatus) => {
         axios.post('https://instamojo.onrender.com/success',paymentStatus)
             .then(res => {
                 let value = res.data.data
                 console.log(value)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, [paymentStatus])
     return (
         <div>
             {paymentStatus === 'Failed' ?  <Link to='/' className='text-center text-red-400 border w-full'><h1> Payment is Unsuccessful back to home page </h1></Link> : <Link to='/' className='text-center text-blue-400 border w-full'><h1> Payment is successful back to home page </h1></Link>}
